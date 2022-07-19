6c06455d7fcc860ec84f085ccbb000c45b1f6005
huangyu <598050554@qq.com>
2022-07-19 17:58:58 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import {createWebHistory, createRouter} from 'vue-router'
+import Home from '../views/map/index.vue'
+
+export const constantRoutes = [
+    {
+        path: '/',
+        component: Home,
+        hidden: true
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: constantRoutes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { top: 0 }
+        }
+    },
+});
+
+export default router;
