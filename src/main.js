90a00076873402891271294598aaa578ae77c6ed
huangyu <598050554@qq.com>
2022-07-19 18:01:48 +0800

    updete

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet'
+import '@geoman-io/leaflet-geoman-free'
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
