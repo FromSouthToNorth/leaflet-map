240bc987077e836067bb493e7c7b059f2e19dbe8
FromSouthToNorth <598050554@qq.com>
2022-07-19 23:39:23 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import './assets/css/reset.css'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet'
 import '@geoman-io/leaflet-geoman-free'
--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,10 +1,29 @@
 <template>
-
+  <div id="map-container"></div>
 </template>
 
 <script setup>
+import {onMounted} from "vue";
+
+let map
+function initMap() {
+  map = L.map('map-container', {
+    crs: L.CRS.EPSG3857,
+    zoom: 10,
+    center: [30.621833394767293, 104.06472467339864]
+  }).setView([30.621833394767293, 104.06472467339864], 9)
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  }).addTo(map);
+}
+onMounted(() => {
+  initMap()
+})
 </script>
 
 <style scoped>
-
+#map-container {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
