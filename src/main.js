28cf50293d6e71c5ba4aa923d848ea5f2d5266a1
huangyu <598050554@qq.com>
2022-07-29 16:55:56 +0800

    add

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import elementIcons from '@/components/SvgIcon/svgicon'
 import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
+import '@/assets/css/index.scss'
 
 import {resetForm} from '@/utils/util.js'
 
--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="map-container">
-  </div>
+  <div id="map-container" />
   <div @click="clearDraw" class="button-clear-draw" title="清除所以绘制">
     <a class="btn-clear-draw" href="#">
       <svg-icon icon-class="delete" color="#434343"/>
@@ -9,75 +8,10 @@
   <div class="map-info">
     <p v-if="mapObj.lng && mapObj.lat">{{ mapObj.lat }}, {{ mapObj.lng }}</p>
   </div>
-  <div v-if="popup.visible" class="map-popup">
-    <el-form ref="mapInfoRef" :model="form" label-width="60px" :rules="rules">
-      <el-form-item label="名称" prop="name">
-        <el-input v-model="form.name"/>
-      </el-form-item>
-      <el-form-item label="颜色" prop="color">
-        <el-color-picker v-model="form.color"/>
-      </el-form-item>
-      <el-form-item label="菜单图标" prop="icon" v-if="mapObj.drawLayer.shape === 'Marker'">
-        <el-popover
-            placement="bottom-start"
-            :width="540"
-            v-model:visible="showChooseIcon"
-            trigger="click"
-            @show="showSelectIcon"
-        >
-          <template #reference>
-            <el-input v-model="form.icon" laceholder="点击选择图标" @click="showSelectIcon" v-click-outside="hideSelectIcon"
-                      readonl>
-              <template #prefix>
-                <svg-icon
-                    v-if="form.icon"
-                    :icon-class="form.icon"
-                    class="el-input__icon"
-                    style="height: 32px;width: 16px;"
-                />
-                <el-icon v-else style="height: 32px;width: 16px;">
-                  <search/>
-                </el-icon>
-              </template>
-            </el-input>
-          </template>
-          <icon-select ref="iconSelectRef" @selected="selected"/>
-        </el-popover>
-      </el-form-item>
-      <el-form-item label="金纬度" prop="latlng" v-if="mapObj.drawLayer.shape === 'Marker'">
-        <el-input-number
-            :precision="15"
-            style="margin-right: 6px; width: 200px"
-            v-model="form.latlng[0]"
-            :min="10"
-            :max="600"
-            size="small"
-            controls-position="right"
-            @change="handleChange"
-        />
-        <el-input-number
-            :precision="15"
-            style="width: 200px"
-            v-model="form.latlng[1]"
-            :min="10"
-            :max="600"
-            size="small"
-            controls-position="right"
-            @change="handleChange"
-        />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">提交</el-button>
-        <el-button @click="onCancel">取消</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
 </template>
 
 <script setup>
-import {getCurrentInstance, onMounted, reactive, ref, toRefs} from 'vue'
-import IconSelect from '@/components/IconSelect/index.vue'
-import {ClickOutside as vClickOutside} from 'element-plus'
+import {getCurrentInstance, onMounted, reactive, ref} from 'vue'
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
 import iconUrl from 'leaflet/dist/images/marker-icon.png'
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
@@ -92,21 +26,7 @@ L.Icon.Default.mergeOptions({
 const {proxy} = getCurrentInstance()
 
 const iconSelectRef = ref(null)
-const showChooseIcon = ref(false)
 
-const popup = reactive({
-  visible: false
-})
-
-const data = reactive({
-  form: {},
-  rules: {
-    name: [{required: true, message: "名称不能为空", trigger: "blur"}],
-    icon: [{required: true, message: "图标不能为空", trigger: "blur"}],
-  },
-})
-
-const {form, rules} = toRefs(data)
 const mapObj = reactive({
   map: undefined,
   lat: undefined,
@@ -155,58 +75,43 @@ function initMap() {
     mapObj.lng = e.latlng.lng
   })
 
-  map.pm.setLang('zh')
-  map.pm.addControls({
-    position: 'topleft',
-  })
-
   map.addControl(L.control.scale({imperial: false}))
 
-  map.on('pm:create', (e) => {
-    onCancel()
-    setDrawData(e)
-    // 绘制后禁用绘制
-    map.pm.disableDraw()
-    popup.visible = true
+  map.locate({
+    setVies: true
   })
+  map.on('locationfound', e => {
+    console.log("locationfound", e.latlng);
+    L.marker(e.latlng).addTo(map).bindPopup("你就在这个圈内")
+  })
+
+  map.on('locationerror', e => {
+    console.log("locationerror --> ", e);
+  })
+
+  // map.pm.setLang('zh')
+  // map.pm.addControls({
+  //   position: 'topleft',
+  // })
+
+
+  // map.on('pm:create', (e) => {
+  //   setDrawData(e)
+  //   // 绘制后禁用绘制
+  //   map.pm.disableDraw()
+  // })
 
   mapObj.map = map
 }
 
 function setDrawData(e) {
   mapObj.drawLayer = e
-  let {shape, layer} = e
-  console.log("shape --> ", shape)
-  form.value.shape = shape
-  if (shape === 'Marker') {
-    form.value.latlng = [layer._latlng.lat, layer._latlng.lng]
-  } else {
-    form.value.latlng = layer._latlngs
-  }
-  console.log("form.value --> ", form.value);
 }
 
 /** 清除所有绘制 */
 function clearDraw() {
   mapObj.map.pm.getGeomanDrawLayers().forEach(e => {
     if (mapObj.map.hasLayer(e)) mapObj.map.removeLayer(e);
-  })
-}
-
-const onSubmit = () => {
-  proxy.$refs['mapInfoRef'].validate(valid => {
-    if (valid) {
-      if (form.value.shape === 'Marker') {
-       const mIcon = L.icon({
-         iconSize: [24, 24],
-         iconUrl: getIconUrl(form.value.icon)
-       })
-        L.marker(form.value.latlng, {
-          icon: mIcon
-        }).addTo(mapObj.map).bindPopup(form.value.name)
-      }
-      onCancel()
-    }
   })
 }
 
@@ -220,42 +125,6 @@ const onCancel = () => {
     mapObj.map.removeLayer(mapObj.drawLayer.layer)
     mapObj.drawLayer = undefined
   }
-  reset()
-}
-
-/** 表单重置 */
-function reset() {
-  popup.visible = false
-  form.value = {
-    name: undefined,
-    color: undefined,
-    icon: undefined,
-    latlng: [],
-    shape: undefined,
-  }
-  proxy.resetForm('mapInfoRef')
-}
-
-/** 展示下拉图标 */
-function showSelectIcon() {
-  iconSelectRef.value.reset();
-  showChooseIcon.value = true;
-}
-
-/** 选择图标 */
-function selected(name) {
-  form.value.icon = name;
-  showChooseIcon.value = false;
-}
-
-/** 图标外层点击隐藏下拉列表 */
-function hideSelectIcon() {
-  showChooseIcon.value = false;
-}
-
-/** 改变经纬度 */
-function handleChange() {
-
 }
 
 onMounted(() => {
@@ -296,24 +165,5 @@ onMounted(() => {
   text-align: center;
   line-height: 30px;
   background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  width: 100%;
-  height: 100%;
-}
-
-.map-popup {
-  width: 500px;
-  z-index: 999;
-  position: absolute;
-  top: 80px;
-  right: 20px;
-  backdrop-filter: blur(6px);
-  background: rgba(255, 255, 255, 0.6);
-  border-radius: 4px;
-  overflow: hidden;
-  padding: 16px;
 }
 </style>
